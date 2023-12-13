68c7855e5f15cc81fb000f0becff5171cbb46e4c
ooherin <555ohr@naver.com>
2023-12-13 20:29:24 +0900

    feat: css 토큰 생성

---
+++ src/style/theme.js
@@ -0,0 +1,41 @@
+const COLORS = {
+  primary: {
+    pink: "FF9DA9",
+    green: "67BD7A",
+    mint: "66EBCB",
+    navy: "#12678C",
+    violet: "#8E29BE",
+  },
+  font: "#0F0F0F",
+  error: "#EF6969",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: {
+    100: "#E4E4E4",
+    200: "#CBCBCB",
+    300: "#8F8F8F",
+    400: "#646464",
+    500: "#3A3A3A",
+  },
+};
+
+const FONT_SIZE = {
+  x_small: "14px",
+  small: "16px",
+  medium: "18px",
+  large: "20px",
+  x_large: "22px",
+};
+
+const FONT_WEIGHT = {
+  thin: 400,
+  regular: 500,
+  bold: 700,
+};
+
+const theme = {
+  COLORS,
+  FONT_SIZE,
+  FONT_WEIGHT,
+};
+export default theme;
